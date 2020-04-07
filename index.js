7a11cd6d6cea62ad0d922e8a6fe058d029b99f0e
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-04-07 17:37:48 +0200

    initial

---
+++ index.js
@@ -0,0 +1,78 @@
+console.log('project is up')
+
+// 1- Create a function called 'get'
+// that takes a CSS selector as its argument
+// and executes document.querySelector with the argument.
+const get = null
+
+
+// 2- Using 'get', select the following elements from the DOM:
+
+// BUTTONS
+const launchButton = null
+const confirmButton = null
+const cancelButton = null
+// MESSAGES
+const successMessage = null
+const failureMessage = null
+// MODAL
+const modal = null
+const modalOpacity = null
+
+
+// 3- Create a function that launches!
+// It should open the confirmation modal.
+// Add it as an event listener for click events on the launch button.
+function launch(event) {
+
+}
+
+
+// 4- Create a function to confirm the launch.
+// It should close the modal and display a success report.
+// Add it as a listener for clicks on the confirmation button.
+function confirm(event) {
+
+}
+
+
+// 5- Create a function to cancel the launch.
+// It should close the modal and display a failure report.
+// Add it as a listener for clicks on the cancellation button.
+function cancel(event) {
+
+}
+
+
+// 6- Create a function that closes the modal if
+// the user hits the Escape key on their keyboard.
+// Add it as an event listener for 'keydown' events on document.
+function escKey(event) {
+
+}
+
+
+// 7- Add to document an event listener for click events.
+// It should console.dir the target of the event.
+// Play with stopPropagation and stopImmediatePropagation
+
+
+// 8- [STRETCH] Create helper functions
+function openModal() {
+
+}
+
+function closeModal() {
+
+}
+
+function killReports() {
+
+}
+
+
+// 9- Using the mouseOver event on 'document',
+// log to the console the X and Y coordinates
+// of the mouse pointer, as it moves over the screen.
+
+// 10- Create a link and prevent its default behavior
